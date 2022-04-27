f3327480d7293a95819a60137c1ad7ab1dba72f0
Kovid63 <97065463+Kovid63@users.noreply.github.com>
2022-04-27 20:18:44 +0530

    Update smartContract.sol

--- smartContract.sol
+++ smartContract.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: MIT
+
+
+// Simple contract for a single item being traded in the marketplace.(without counters and tokenURI/Metadata)
+
 pragma solidity ^0.8.4;
 
 contract myContract {
@@ -28,7 +32,7 @@ contract myContract {
     function itemSold() public payable {
         require(msg.value == price,"Not the correct price");
         nftOwner.transfer(msg.value);
-
+        owner.transfer(listingPrice);
     } 
    
 }
